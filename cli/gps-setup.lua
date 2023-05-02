5b6f2fce04a9f3fac317a8183f6fba40e3065030
Timo Hocker <35867059+TimoHocker@users.noreply.github.com>
2023-05-02 22:31:45 +0200

    gps host setup

---
+++ cli/gps-setup.lua
@@ -0,0 +1,9 @@
+if not fs.exists("startup") then
+  fs.makeDir("startup")
+end
+
+local args = {...}
+local file = fs.open("startup/gps-host.lua", "w")
+file.write("print(\"starting gps host\")")
+file.write("shell.run(\"gps host " .. args[1] .. " " .. args[2] .. " " .. args[3] .. "\")")
+file.close()
