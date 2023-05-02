c1543702c513541c5e378b62ec0342789dc710a4
Timo Hocker <35867059+TimoHocker@users.noreply.github.com>
2023-05-02 22:22:56 +0200

    show messages

--- cli/git.lua
+++ cli/git.lua
@@ -6,8 +6,10 @@ local cmd = args[1]
 
 if cmd == "pull" then
   table.remove(args, 1)
+  print("Pulling file " .. args[1])
   git.pull(table.unpack(args))
 end
 if cmd == "update" then
   git.update_git()
+  print("Updated git")
 end
