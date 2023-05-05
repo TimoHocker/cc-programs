a60357a713e35dc2176d961396ccbb5f75a4f993
Timo Hocker <35867059+TimoHocker@users.noreply.github.com>
2023-05-05 13:44:09 +0200

    oop turtle api

--- apis/ant-turtle.lua
+++ apis/ant-turtle.lua
@@ -1,45 +1,67 @@
-function start(modem, name)
-  if not turtle then
-    error("This is not a turtle")
-  end
-  name = name or os.getComputerLabel()
-  if name == nil then
-    error("No name provided and no computer label set")
-  end
-  print("Starting ant with name " .. name)
-  rednet.open(modem)
-  if (rednet.lookup("ant", name) ~= nil) then
-    error("Ant with name " .. name .. " already exists")
-  end
-  print("Registering hostname " .. name)
-  rednet.host("ant", name)
-  print("locating turtle with gps")
-  local x,y,z = gps.locate(2, true)
-  if x == nil then
-    error("Could not locate turtle")
-  end
-  local position = vector.new(x,y,z)
-  rednet.broadcast("login " .. name .. " " .. position:tostring(), "ant")
-  return { modem = modem, name = name, position = position }
-end
+Ant = {
+  new = function(self, modem, name)
+    if not turtle then
+      error("This is not a turtle")
+    end
 
-function run(config)
-  while true do
-    local id, message = rednet.receive("ant")
+    local o = {}
+    setmetatable(o, { __index = self })
+    o.modem = modem
+    o.name = name or os.getComputerLabel()
+    if name == nil then
+      error("No name provided and no computer label set")
+    end
+    o.location = vector.new(0, 0, 0)
+    o.active = false
+    o.running = false
+    return o
+  end,
+
+  start = function(self)
+    print("Starting ant with name " .. self.name)
+    rednet.open(self.modem)
+    if (rednet.lookup("ant", self.name) ~= nil) then
+      error("Ant with name " .. self.name .. " already exists")
+    end
+    print("Registering hostname " .. self.name)
+    rednet.host("ant", self.name)
+    print("locating turtle with gps")
+    local x, y, z = gps.locate(2, true)
+    if x == nil then
+      error("Could not locate turtle")
+    end
+    self.position = vector.new(x, y, z)
+    rednet.broadcast("login " .. self.name .. " " .. self.position:tostring(), "ant")
+  end,
+
+  run = function(self)
+    self.running = true
+    self.active = true
+    while self.active do
+      local id, message = rednet.receive("ant", 5)
+      if id and self.active then
+        self:handle_message(id, message)
+      end
+    end
+    self.running = false
+  end,
+
+  handle_message = function(self, id, message)
     if message == "ping" then
       print("Received ping from " .. id)
-      rednet.send(id, "pong " .. config.name .. " " .. config.position:tostring(), "ant")
+      rednet.send(id, "pong " .. self.name .. " " .. self.position:tostring(), "ant")
     end
+  end,
+
+  stop = function(self)
+    self.active = false
+    while self.running do
+      sleep(0.1)
+    end
+    rednet.broadcast("logout " .. self.name, "ant")
+    rednet.unhost("ant", self.name)
+    rednet.close(self.modem)
   end
-end
-
-function stop(config)
-  rednet.unhost("ant", config.name)
-  rednet.close(config.modem)
-end
-
-return {
-  start = start,
-  run = run,
-  stop = stop
 }
+
+return Ant
